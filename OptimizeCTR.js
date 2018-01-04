50fececb28cdfe21313171cc0146ba5f0dbfa6f0
Eric King <cg100146@comporium.com>
2018-01-04 10:21:37 -0500

    initial script coded, need to setup spreadsheet

--- OptimizeCTR.js
+++ OptimizeCTR.js
@@ -10,6 +10,65 @@
 // Conditional check - If CPM >$1 = ignore
 
 function main() {
+    // Select account
     var currentAccount = AdWordApp.currentAccount();
     var accountName = currentAccount.getName();
+
+    // Init Variables
+    var CtrCheck = .00010;
+    var CpmAmount = 1;
+
+    // Spreadsheet init
+    var spreadsheet_url = '';
+    var spreadsheet = SpreadsheetApp.openByUrl(spreadsheet_url);
+    var sheet = spreadsheet.getSheets()[0];
+
+    // Grab SS Data
+    var emailForNotify = sheet.getRange();
+
+    // Email function to pass string and send through to email provided
+    function notify(string) {
+        MailApp.sendEmail(emailForNotify, accountName, string);
+    }
+
+    // Select campaign - Grab stats in loop
+    var campaignSelector = AdWordsApp
+        .campaigns()
+        .withCondition("Status = ENABLED");
+    
+    var campaignIterator = campaignSelector.get();
+    while(campaignIterator.hasNext()) {
+        var campaign = campaignIterator.next();
+        Logger.log("Campaign: " + campaign.getName());
+
+        var currentBiddingStrategy = campaign.getBiddingStrategyType();
+        Logger.log("current bidding strategy: " + currentBiddingStrategy);
+
+        var currentStats = campaign.getStatsFor("LAST_7_DAYS");
+        var currentCost = currentStats.getCost();
+        var currentClicks = currentStats.getClicks();
+        var currentCpm = currentStats.getAverageCpm();
+        var currentCtr = currentStats.getCtr();
+        var currentCpc = currentStats.getCpc();
+    }
+    // Select ad group to adjust cpm
+    var adGroupSelector = AdWordApp
+        .adGroups();
+
+    var adGroupIterator = adGroupSelector.get();
+    while(adGroupIterator.hasNext()) {
+        var adGroup = adGroupIterator.next();
+    }
+    
+
+    // Conditional check - If CPM >$1 = ignore
+    if(currentCpm > CpmAmount) {
+        Logger.log("Campaign CTR in a good place");
+    }
+
+    // Conditional check - If CTR is > .10% raise CPM to CPMamount
+    if(currentCtr > CtrCheck) {
+        Logger.log("CTR not in a good spot - adjusting...");
+        adGroup.setCpm(CpmAmount);
+    }
 }
